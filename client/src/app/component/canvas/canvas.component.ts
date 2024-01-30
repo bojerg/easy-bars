d807f5052ccf04a03272bdfa4892632c1844d3c7
bojerg <jcbjoe99@gmail.com>
2024-01-30 17:51:42 -0600

    Adding core canvas functionality

--- client/src/app/component/canvas/canvas.component.ts
+++ client/src/app/component/canvas/canvas.component.ts
@@ -1,7 +1,11 @@
 import { Component, HostListener, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Page } from '../../model/page';
 import { PageComponent } from '../page/page.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { CdkDragStart, DragDropModule } from '@angular/cdk/drag-drop';
 import { Canvas } from '../../model/canvas';
 import { ProjectService } from '../../service/project.service';
 import { Subscription } from 'rxjs';
@@ -9,7 +13,7 @@ import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-canvas',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, DragDropModule, CommonModule],
   templateUrl: './canvas.component.html',
   styleUrl: './canvas.component.scss'
 })
@@ -26,11 +30,16 @@ export class CanvasComponent {
   canvasSub: Subscription = new Subscription;
   canvas!: Canvas;
 
+  dragStart: any;
+  dragIndex: number = -1;
+
   constructor(private dialog: MatDialog, private projectService: ProjectService) {}
 
   ngOnInit() {
     this.projectService.canvas.subscribe(canvas => this.canvas = canvas);
     this.calculateBarsPerRow();
+
+
 
     // temporary number in lieu of object oriented approach
     for(let i = 1; i <= 17; i++) {
@@ -66,4 +75,21 @@ export class CanvasComponent {
     this.dialog.open(PageComponent, config);
   }
 
+  getCardWidth(index: number): number {
+    const duration = this.canvas.tracks[index].getFullDuration();
+    return duration > 4 ? duration * 44 : 176;
+  }
+
+  // https://stackoverflow.com/questions/70385721/angular-material-cdk-drag-and-drop-snap-to-grid-internal-element-cdkdragconstrai
+  dragStarted(event: CdkDragStart, index: number): void {
+      this.dragStart = event.source.element.nativeElement.getBoundingClientRect();
+      this.dragIndex = index;
+  }
+  computeDragRenderPos(pos: any, _: any): {x: number, y: number} {
+      // will render the element every 22 pixels horizontally
+      const delta = pos.x - this.dragStart.x;
+      let xPos = this.dragStart.x + Math.floor(delta / 22) * 22;
+      this.canvas.tracks[this.dragIndex].start = (xPos - 112) / 44;
+      return {x: xPos, y: this.dragStart.y};
+  }
 }
--- client/src/app/model/page.ts
+++ client/src/app/model/page.ts
@@ -4,10 +4,35 @@ export class Page {
     id: number;
     title: string;
     lyrics: Phrase[];
+    start: number;
 
     constructor(id: number, title: string, lyrics: Phrase[]) {
         this.id = id;
         this.title = title;
         this.lyrics = lyrics;
+        this.start = 0;
+    }
+
+    getFullDuration(): number {
+        let duration = 0;
+        this.lyrics.forEach( phrase => duration += phrase.duration);
+        return duration;
+    }
+
+    getPreview(): string {
+        let preview = "";
+        if(this.lyrics.length > 0 && this.lyrics[0].content !== "") {
+            const content = this.lyrics[0].content;
+            preview = content.length < 13 ? content : content.substring(0, 12);
+        } else if(this.lyrics.length > 1) {
+            for(let i = 1; i < this.lyrics.length; i++) {
+                if(this.lyrics[i].content !== "") {
+                    const content = this.lyrics[i].content;
+                    preview = content.length < 13 ? content : content.substring(0, 12);
+                    break;
+                }
+            }
+        }
+        return preview === "" ? "Empty page" : preview + " ...";
     }
 }
