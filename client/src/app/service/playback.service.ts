4a2711f87e6bd4307d517f3766ecd33239b8ad8e
bojerg <jcbjoe99@gmail.com>
2024-03-14 20:29:18 -0500

    added beat tracking interval to playback service

--- client/src/app/service/playback.service.ts
+++ client/src/app/service/playback.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, interval } from 'rxjs';
 import { Playback } from '../model/playback';
 
 @Injectable({
@@ -12,44 +12,67 @@ export class PlaybackService {
 
   metronome = new Howl({src: ['../../../assets/metronome_1.mp3'], sprite: {"metronome": [0, 600, true]}});
   instrumental: Howl | undefined;
+  intervalLenth = 37.5; // 1/16th beat at 100bpm
+  intervalId: any = null;
 
   play(playback: Playback): void {
     playback.playing = true;
-    this.playbackSource.next(playback);
     if(playback.metronome) this.metronome.play("metronome");
     if(this.instrumental !== undefined) this.instrumental.play();
+    this.intervalId = setInterval(() => this.iterateBeats(playback), this.intervalLenth);
   }
 
   pause(playback: Playback): void {
     playback.playing = false;
     playback.paused = true;
-    this.playbackSource.next(playback);
     if(playback.metronome) this.metronome.pause();
     if(this.instrumental !== undefined) this.instrumental.pause();
+    this.clearInterval();
   }
 
   stop(playback: Playback): void {
     playback.playing = false;
     playback.paused = false;
-    this.playbackSource.next(playback);
     if(playback.metronome) this.metronome.stop();
     if(this.instrumental !== undefined) this.instrumental.stop();
+    this.resetInterval(playback);
   }
 
   setBpm(bpm: number, playback: Playback): void {
     playback.bpm = bpm;
     playback.playing = false;
     playback.paused = false;
-    this.playbackSource.next(playback);
-    const newDuration = 60000 / bpm;
+    this.resetInterval(playback);
     this.metronome.stop();
     if(this.instrumental !== undefined) this.instrumental.stop();
+    const newDuration = 60000 / bpm;
     this.metronome = new Howl({src: ['../../../assets/metronome_1.mp3'], sprite: {"metronome": [0, newDuration, true]}});
+    this.intervalLenth = newDuration / 16;
   }
 
   setMetronome(setting: boolean, playback: Playback): void {
     playback.metronome = setting;
     this.playbackSource.next(playback);
+  }
+
+  private iterateBeats(playback: Playback): void {
+    playback.beat += 0.0625;
+    if(playback.beat >= 4) {
+      playback.beat = 0;
+      playback.bar++;
+    }
+    this.playbackSource.next(playback);
+  }
+
+  private resetInterval(playback: Playback): void {
+    this.clearInterval();
+    playback.beat = 0;
+    playback.bar = 0;
+    this.playbackSource.next(playback);
+  }
+
+  private clearInterval(): void {
+    if(this.intervalId !== null) clearInterval(this.intervalId);
   }
 
 }
